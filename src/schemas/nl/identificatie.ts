2dd7d026e8509c12a91f4e37bf625b29825f8995
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-08-10 20:42:12 +0200

    q5 enum opties in aparte file

--- src/schemas/nl/identificatie.ts
+++ src/schemas/nl/identificatie.ts
@@ -1,3 +1,5 @@
+import { q5 } from "./identificatie/q5";
+
 export const identificationSchema = {
   JSONSchema: {
     title: "Identificatie AI-systeem en impactvolle algoritmes",
@@ -327,21 +329,7 @@ export const identificationSchema = {
                   q4: {
                     enum: ["Ja"],
                   },
-                  q5: {
-                    type: "string",
-                    title:
-                      "Wat voor soort beslissing wordt er genomen in dit proces?\n\nKies de optie die de grootste overeenkomst heeft met de soort beslissing.",
-                    enum: [
-                      "Beslissing over prioritering van aanvragen, verzoeken, klachten en bezwaren",
-                      "Beslissing over formele klachten en bezwaren",
-                      "Beslissing met directe financiële gevolgen voor burger of ambtenaar, \nzoals bijvoorbeeld beslissingen over een arbeidscontract, uitkering, toeslag, subsidie, boete, terugbetaling of mogelijkheid tot betalingsregeling",
-                      "Beslissing over controle, onderzoek of verzoek tot aanvullende informatieverschaffing",
-                      "Beslissing over toewijzing van scholen",
-                      "Beslissingen over te geven advies of het (proactief) aanbieden van diensten of voorzieningen",
-                      "Overige of andere beslissingen",
-                      "Een andere beslissing met rechtsgevolgen zoals bijvoorbeeld vergunning toekenning of het aangaan van een overeenkomst",
-                    ],
-                  },
+                  q5,
                 },
                 required: ["q5"],
               },
@@ -513,22 +501,7 @@ export const identificationSchema = {
                   q4: {
                     enum: ["Ja"],
                   },
-                  q5: {
-                    type: "string",
-                    title:
-                      "Wat voor soort beslissingen worden genomen in dit proces? \nKies de optie die de grootste overeenkomst heeft met de soort beslissing.",
-                    enum: [
-                      "Beslissing over prioritering van aanvragen, verzoeken, klachten en bezwaren",
-                      "Beslissing over formele klachten en bezwaren",
-                      "Beslissing met directe financiële gevolgen voor burger of ambtenaar, \nzoals bijvoorbeeld beslissingen over een arbeidscontract, uitkering, toeslag, subsidie, boete, terugbetaling of mogelijkheid tot betalingsregeling",
-                      "Beslissing over aanvragen en verzoeken zonder directe financiële gevolgen, \nzoals bijvoorbeeld toekenning van een aanvraag van diensten of vergunningtoekenning",
-                      "Beslissing over controle, onderzoek of verzoek tot aanvullende informatieverschaffing",
-                      "Beslissing over toewijzing van scholen",
-                      "Beslissingen over te geven advies of het (proactief) aanbieden van diensten of voorzieningen",
-                      "Overige of andere beslissingen",
-                      "Een andere beslissing met rechtsgevolgen zoals bijvoorbeeld vergunning toekenning of het aangaan van een overeenkomst",
-                    ],
-                  },
+                  q5,
                 },
                 required: ["q5"],
               },
---
+++ src/schemas/nl/identificatie/q5.ts
@@ -0,0 +1,15 @@
+export const q5 = {
+  type: "string",
+  title:
+    "Wat voor soort beslissing wordt er genomen in dit proces?\n\nKies de optie die de grootste overeenkomst heeft met de soort beslissing.",
+  enum: [
+    "Beslissing over prioritering van aanvragen, verzoeken, klachten en bezwaren",
+    "Beslissing over formele klachten en bezwaren",
+    "Beslissing met directe financiële gevolgen voor burger of ambtenaar, \nzoals bijvoorbeeld beslissingen over een arbeidscontract, uitkering, toeslag, subsidie, boete, terugbetaling of mogelijkheid tot betalingsregeling",
+    "Beslissing over controle, onderzoek of verzoek tot aanvullende informatieverschaffing",
+    "Beslissing over toewijzing van scholen",
+    "Beslissingen over te geven advies of het (proactief) aanbieden van diensten of voorzieningen",
+    "Overige of andere beslissingen",
+    "Een andere beslissing met rechtsgevolgen zoals bijvoorbeeld vergunning toekenning of het aangaan van een overeenkomst",
+  ],
+};
