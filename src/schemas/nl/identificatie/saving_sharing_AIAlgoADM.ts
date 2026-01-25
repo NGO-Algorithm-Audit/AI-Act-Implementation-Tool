ebb6e3678baec6fb4049069afa10fa366d71fe18
jfparie <jfparie@me.com>
2026-01-25 11:09:26 +0100

    NL capitalized all "ja", "nee" antwoorden

--- src/schemas/nl/identificatie/saving_sharing_AIAlgoADM.ts
+++ src/schemas/nl/identificatie/saving_sharing_AIAlgoADM.ts
@@ -7,7 +7,7 @@ export const saving_sharing_AIAlgoADM = {
       type: "string",
       title:
         "Gaat de output van het algoritme over een individuele burger of een casus die een individuele burger aangaat?",
-      enum: ["ja", "nee"],
+      enum: ["Ja", "Nee"],
     },
   },
   dependencies: {
@@ -16,7 +16,7 @@ export const saving_sharing_AIAlgoADM = {
         {
           properties: {
             q8: {
-              enum: ["nee"],
+              enum: ["Nee"],
             },
             outputIntermediate: {
               type: "string",
@@ -32,13 +32,13 @@ export const saving_sharing_AIAlgoADM = {
         {
           properties: {
             q8: {
-              enum: ["ja"],
+              enum: ["Ja"],
             },
             q9: {
               type: "string",
               title:
                 "Wordt de output van het algoritme gedeeld met andere organisaties?",
-              enum: ["ja", "nee"],
+              enum: ["Ja", "Nee"],
             },
           },
           required: ["q9"],
@@ -48,7 +48,7 @@ export const saving_sharing_AIAlgoADM = {
                 {
                   properties: {
                     q9: {
-                      enum: ["ja"],
+                      enum: ["Ja"],
                     },
                     additionalOutputText: {
                       default: `Let op: Het langdurig opslaan of delen met derden van de output van een algoritme kan leiden tot aanmerkelijke (onvoorziene) gevolgen voor een betrokkene. Hierdoor kan in juridische zin sprake zijn van geautomatiseerde besluitvorming (art 22) ook wanneer het primaire gebruik van het algoritme dit niet is. Bepaal in overleg met relevante juridische experts of door opslag of delen van de uitkomsten indirect sprake kan zijn van geautomatiseerde besluitvorming (art 22). Zie ook https://www.autoriteitpersoonsgegevens.nl/documenten/advies-geautomatiseerde-besluitvorming`,
@@ -67,13 +67,13 @@ export const saving_sharing_AIAlgoADM = {
                 {
                   properties: {
                     q9: {
-                      enum: ["nee"],
+                      enum: ["Nee"],
                     },
                     q10: {
                       type: "string",
                       title:
                         "Wordt de output van het algoritme langer opgeslagen dan de doorlooptijd van het primaire proces waarvoor het algoritme wordt ingezet?",
-                      enum: ["ja", "nee"],
+                      enum: ["Ja", "Nee"],
                     },
                   },
                   required: ["q10"],
@@ -83,7 +83,7 @@ export const saving_sharing_AIAlgoADM = {
                         {
                           properties: {
                             q10: {
-                              enum: ["ja"],
+                              enum: ["Ja"],
                             },
                             additionalOutputText: {
                               default: `Let op: Het langdurig opslaan of delen met derden van de output van een algoritme kan leiden tot aanmerkelijke (onvoorziene) gevolgen voor een betrokkene. Hierdoor kan in juridische zin sprake zijn van geautomatiseerde besluitvorming (art 22) ook wanneer het primaire gebruik van het algoritme dit niet is. Bepaal in overleg met relevante juridische experts of door opslag of delen van de uitkomsten indirect sprake kan zijn van geautomatiseerde besluitvorming (art 22). Zie ook https://www.autoriteitpersoonsgegevens.nl/documenten/advies-geautomatiseerde-besluitvorming`,
@@ -102,7 +102,7 @@ export const saving_sharing_AIAlgoADM = {
                         {
                           properties: {
                             q10: {
-                              enum: ["nee"],
+                              enum: ["Nee"],
                             },
                             outputIntermediate: {
                               type: "string",
